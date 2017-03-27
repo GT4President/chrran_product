72ad84e7450a887d89b53afd082d1ee1fc574e46
GT4President <ran17930@spengergasse.at>
2017-03-27 20:05:26 +0200

    last commit

--- Product.java
+++ Product.java
@@ -2,11 +2,13 @@ public class Product {
 private String name;
 private float price;
 private int id;
+private boolean onStock;
 
 public Product(int id) {
 	name = "Coca Cola";
 	price = 10;
 	this.id = id;
+	onStock = true;
 }
 
 public String getName() {
